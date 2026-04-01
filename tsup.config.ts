5bb39b7f60a63b928e33ab9fed53bad50882d806
themaker <michthebrand@gmail.com>
2026-04-01 11:45:48 +0100

    feat(tsup.config.ts) optimized build for dom specific lib

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: ["src/index.ts", "src/node/index.ts"],
-  format: ["esm"],
-  dts: true,
-  outDir: "dist",
-  clean: true,
-  skipNodeModulesBundle: true,
-});
+export default defineConfig([
+  {
+    entry: ["src/index.ts", "src/node/index.ts"],
+    format: ["esm"],
+    dts: true,
+    outDir: "dist",
+    clean: false,
+    skipNodeModulesBundle: true,
+  },
+  {
+    entry: ["src/dom/index.ts"],
+    format: ["esm"],
+    dts: { resolve: false },
+    outDir: "dist/dom",
+    clean: false,
+    skipNodeModulesBundle: true,
+    tsconfig: "src/dom/tsconfig.json",
+    external: ["../index"],
+  },
+]);
